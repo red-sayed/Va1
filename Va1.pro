12fb10212c7090e2b7d0cde1b5b495c4a6efd77d
Vladimir Rogozin <vladimir20040609@gmail.com>
2021-10-31 00:31:25 +0300

    Initialization

---
+++ Va1.pro
@@ -0,0 +1,11 @@
+TEMPLATE = app
+CONFIG += console c++11
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += \
+        Va1/Va1.cpp \
+        main.cpp
+
+HEADERS += \
+    Va1/Va1.h
---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+
+#include "Va1/Va1.h"
+
+int main() {
+    std::string inp;
+
+    while (true) {
+        system("clear");
+        std::cout << "\033[4mChoose operation:\033[0m 1)Encode 2)Decode." << std::endl << std::endl << "[\033[93mVa1\033[0m]  > ";
+
+        std::cin >> inp;
+
+        // Encrypting.
+        if (inp == "1") {
+            system("clear");
+            std::cout << "\033[4mEnter a message.\033[0m" << std::endl << std::endl << "[\033[93mVa1\033[0m] > ";
+
+            std::cin.ignore();
+            inp.clear();
+            getline(std::cin, inp);
+            std::cout << std::endl;
+
+            std::cout << "[\033[93mVa1\033[0m] \033[4mEncrypted message:\033[0m" << std::endl << std::endl << Red::Va1Encode(inp);
+            std::cout << std::endl << std::endl << std::endl << "Press Enter to continue.";
+
+            std::cin.get();
+
+        // Decrypting.
+        } else if (inp == "2") {
+            system("clear");
+            std::cout << "\033[4mEnter a message.\033[0m" << std::endl << std::endl << "[\033[93mVa1\033[0m] > ";
+
+            std::cin.ignore();
+            inp.clear();
+            getline(std::cin, inp);
+            std::cout << std::endl;
+
+            std::cout << "[\033[93mVa1\033[0m] Decrypted message:" << std::endl << std::endl << Red::Va1Decode(inp);
+            std::cout << std::endl << std::endl << std::endl << "Press Enter to continue.";
+
+            std::cin.get();
+        }
+    }
+}
