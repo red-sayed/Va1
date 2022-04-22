969b4dbe4149ce7b3025d03c0c0f9819bed35772
Vladimir Rogozin <vladimir20040609@gmail.com>
2022-04-22 13:36:02 +0300

    edit

--- Va1/Va1.cpp
+++ Va1/Va1.cpp
@@ -178,7 +178,7 @@ class _red_for_Va1 {
                 case '?':
                     *Fstep += "51";
                     break;
-                case '\"':
+                case '\\':
                     *Fstep += "52";
                     break;
                 case ' ':
@@ -300,12 +300,6 @@ class _red_for_Va1 {
                     break;
                 case '/':
                     *Fstep += "92";
-                    break;
-                case '\\':
-                    *Fstep += "93";
-                    break;
-                case '\'':
-                    *Fstep += "94";
                     break;
                 default:
                     *Fstep += "4242"; // point.
@@ -453,7 +447,7 @@ class _red_for_Va1 {
                     } else if ((*RLast)[symb+1] == '1') {
                         *Lstep += "?";
                     } else if ((*RLast)[symb+1] == '2') {
-                        *Lstep += "\"";
+                        *Lstep += "\\";
                     } else if ((*RLast)[symb+1] == '3') {
                         *Lstep += " ";
                     } else if ((*RLast)[symb+1] == '4') {
@@ -556,8 +550,6 @@ class _red_for_Va1 {
                         *Lstep += "/";
                     } else if ((*RLast)[symb+1] == '3') {
                         *Lstep += "\\";
-                    } else if ((*RLast)[symb+1] == '4') {
-                        *Lstep += "\'";
                     }
 
                     break;
